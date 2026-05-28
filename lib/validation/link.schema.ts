9ae6ff80b1ef65e99bea45b7d1953b1323793457
YawAsanteBoadu <164598357+YawAsanteBoadu@users.noreply.github.com>
2026-05-28 15:38:02 +0000

    feat: implement unpublish functional in add and edit link modal (#50)
    Co-authored-by: Joel Osei Acquah <oseijoel6111@gmail.com>

--- lib/validation/link.schema.ts
+++ lib/validation/link.schema.ts
@@ -2,6 +2,9 @@ import {z} from "zod"
 
 export const addLinkSchema = z.object({
     title: z.string().min(1, "Title is required"),
-    url: z.string().url("Must be a valid URL"),
+    url: z.url().min(1, "Must be a valid URL"),
     icon: z.string().optional(),
+    status: z.number().int().min(0).max(2).optional(),
+    thumbnailUrl: z.string().optional(),
+    thumbnailKey: z.string().optional(),
 });
